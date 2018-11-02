c8e1626227b77f807146ec02aae2e7dc2a509a78
mikkujain <mayankj914@gmail.com>
2018-11-02 15:06:58 +0530

    change layout like data_logger

--- users/views.py
+++ users/views.py
@@ -6,6 +6,9 @@ from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from graphos.sources.simple import SimpleDataSource
+from graphos.renderers.gchart import LineChart
+
 from django.contrib.auth.models import User
 
 def Home(request):
