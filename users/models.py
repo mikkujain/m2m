550426a67cce4cf00cc7b5aaa6e970b11fa285a9
mikkujain <mayankj914@gmail.com>
2018-11-03 16:40:16 +0530

    make front-end

--- users/models.py
+++ users/models.py
@@ -7,14 +7,15 @@ from django.utils.timezone import datetime
 from datetime import date
 # Create your models here.
 class Site(models.Model):
+	site_id = models.IntegerField(default=0)
 	directory = models.CharField(max_length=250)
 	folder = models.CharField(max_length=100)
 
 	def __str__(self):
-		return '{}/{}'.format(self.directory, self.folder)	
+		return '{} id {}/{}'.format(self.site_id, self.directory, self.folder)	
 
 class Files(models.Model):
-	site = models.ForeignKey(Site)
+	site = models.ForeignKey(Site, on_delete=models.CASCADE)
 	name = models.CharField(max_length=255)
 	date = models.DateField(default=date.today)
 
@@ -22,11 +23,10 @@ class Files(models.Model):
 		return '{} at {}'.format(self.name, self.date)
 
 class Data(models.Model):
-	file = models.ForeignKey(Files)
+	file = models.ForeignKey(Files, on_delete=models.CASCADE)
 	volt = models.FloatField(null=True, blank=True, default=0)
 	level = models.FloatField(null=True, blank=True, default=0)
 	datetime = models.DateTimeField()
 
 	def __str__(self):
-		return '{} {} {} at {}'.format(self.file, self.volt, self.level, self.datetime)
-
+		return '{} volt={} level={} at {}'.format(self.file, self.volt, self.level, self.datetime)
--- users/views.py
+++ users/views.py
@@ -20,6 +20,8 @@ from datetime import date, datetime
 from graphos.sources.simple import SimpleDataSource
 from graphos.renderers import gchart
 
+today = date.today()
+
 def Home(request):
     return render(request, 'home.html')
 
@@ -31,22 +33,116 @@ class Dashboard(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
 		queryset = Data.objects.all()
-		# print(queryset)
+		print(queryset)
 		return queryset
 
     def get_context_data(self, **kwargs):
 		context = super(Dashboard, self).get_context_data(**kwargs)
+		# context["data_list"] = context["data_list"].filter(datetime__year=today.year, datetime__month=today.month, datetime__day=today.day).order_by("datetime")
+		# # print(context['data_list'])
+
+		# dv = Data.objects.all()
+		# # # print(dv)
+		# context["device3"] = context["data_list"].filter(dv[0])
+		# context["device4"] = context["data_list"].filter(file__site=dv[1])
+		# context["device5"] =
+		# context["device6"] =
+		# # print("device 1 is", context)
+		#  print(context['device2'])
+
 		startd = self.request.GET.get('start-date')
-		endd = self.request.GET.get('end-date')
+		endd   = self.request.GET.get('end-date')
 		if self.request.GET.get('date'):
 			try:
 				today = datetime.strptime(self.request.GET.get('date'), '%Y-%m-%d').date()
 				context["data_list"] = context["data_list"].filter(datetime__year=today.year, datetime__month=today.month, datetime__day=today.day).order_by("datetime")
-				dv = Files.objects.all()
-				context["device1"]   = context["data_list"].filter(file__name=dv[0].name)
-				context["device2"] 	 = context["data_list"].filter(file__name=dv[1])
-				print("context is ", context['device1'])
+				dv = Site.objects.all()
+				context["device1"]   = context["data_list"].filter(file__site=dv[0])
+				context["device2"] 	 = context["data_list"].filter(file__site=dv[1])
 			except Exception as e:
 				context['errors'] = ["Invalid Date Format Selected"]
+		elif startd and endd:
+			try:
+				startd = datetime.strptime(startd, '%Y-%m-%d').date()
+				endd = datetime.strptime(endd, '%Y-%m-%d').date()
+				context["data_list"] = context["data_list"].filter(datetime__date__range=[startd, endd])
+				dv = Site.objects.all()
+				context["device1"]   = context["data_list"].filter(file__site=dv[0])
+				context["device2"] 	 = context["data_list"].filter(file__site=dv[1])
+			except Exception as e:
+				context['errors'] = ["Invalid Date Format Selected"]
+		else:
+			today = datetime.today()
+			context["data_list"] = context["data_list"].filter(datetime__year=today.year, datetime__month=today.month, datetime__day=today.day).order_by("datetime")
+			dv = Site.objects.all()
+			context["device1"] = context["data_list"].filter(file__site=dv[0])
+			context["device2"] = context["data_list"].filter(file__site=dv[1])
 
-		return context
+		# # print(context)
+		# if self.request.GET.get('date'):
+		# 	try:
+		# 		today = datetime.strptime(self.request.GET.get('date'), '%Y-%m-%d').date()
+		# 		context["data_list"] = context["data_list"].filter(datetime__year=today.year, datetime__month=today.month, datetime__day=today.day).order_by("datetime")
+		# 		# print(context['data_list'])
+		# 		dv = Site.objects.all()
+		# 		# print(dv)
+		# 		print(context['data_list'])
+		# 		context["device1"]   = context["data_list"].filter(file__name=dv[0].name)
+		# 		context["device2"] 	 = context["data_list"].filter(file__name=dv[1])
+		# 		print("context is ", context['device1'])
+		# 	except Exception as e:
+		# 		context['errors'] = ["Invalid Date Format Selected"]
+
+		# return context
+
+		data = []
+		data1 = []
+		r1 = []
+		r2 = []
+		r3 = []
+		r4 = []
+		if context['device1']:
+			data = [['DateTime', 'Water_Level']]
+			for i in context['device1']:
+				data.append([i.datetime.strftime("%d/%m/%y %H:%I %p"), i.level])
+			chart = gchart.ColumnChart(SimpleDataSource(data=data), options={'title': 'NHPC_Sewa'}, height=400, width=600)
+			context["chart"] = chart
+		# return context
+
+		if context['device2']:
+			data1 = [['DateTime','Water_Level']]
+			for i in context['device2']:
+				data1.append([i.datetime.strftime("%d/%m/%y %H:%I %p"),i.level])
+			chart1 = gchart.ColumnChart(SimpleDataSource(data=data1), options={'title': 'NHPC_Sewa'}, height=400, width=600)
+			context["chart1"] = chart1
+
+		# if context['device3']:
+		r1 = [['DateTime', 'Rain_Fall']]
+		for i in range(5):
+			r1.append([0, 0])
+		chart2 = gchart.ColumnChart(SimpleDataSource(data=r1), options={'title': 'NHPC_Sewa'}, height=400, width=600)
+		context["chart2"] = chart2
+
+
+		# if context['device4']:
+		r2 = [['DateTime', 'Rain_Fall']]
+		for i in range(5):
+			r2.append([0,0])
+		chart3 = gchart.ColumnChart(SimpleDataSource(data=r2), options={'title': 'NHPC_Sewa'}, height=400, width=600)
+		context["chart3"] = chart3
+
+		# if context['device5']:
+		r3 = [['DateTime', 'Rain_Fall']]
+		for i in range(5):
+			r3.append([0,0])
+		chart4 = gchart.ColumnChart(SimpleDataSource(data=r3), options={'title': 'NHPC_Sewa'}, height=400, width=600)
+		context["chart4"] = chart4
+
+		# if context['device6']:
+		r4 = [['DateTime', 'Rain_Fall']]
+		for i in range(5):
+			r4.append([0,0])
+		chart5 = gchart.ColumnChart(SimpleDataSource(data=r4), options={'title': 'NHPC_Sewa'}, height=400, width=600)
+		context["chart5"] = chart5
+
+		return context		
