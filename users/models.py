06279a4ab00eeaf6c565308110374913ed83f4e7
mikkujain <mayankj914@gmail.com>
2018-11-05 12:56:51 +0530

    change in site model

--- users/models.py
+++ users/models.py
@@ -7,7 +7,6 @@ from django.utils.timezone import datetime
 from datetime import date
 # Create your models here.
 class Site(models.Model):
-	site_id = models.IntegerField(default=0)
 	directory = models.CharField(max_length=250)
 	folder = models.CharField(max_length=100)
 
